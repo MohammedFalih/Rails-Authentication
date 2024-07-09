7d1350d591eaf9d9b933c032a04d4c5e43717356
Mohammed Falih <yunusfalih123@gmail.com>
2024-07-09 16:10:20 +0530

    final commit

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
     if (user = User.find_by(email: params[:email]))
         PasswordMailer.with(
             user: user,
-            token: user.generates_token_for(:password_reset)
+            token: user.generate_token_for(:password_reset)
         ).password_reset.deliver_later
     end
 
@@ -30,10 +30,10 @@ class PasswordResetsController < ApplicationController
 
   def set_user_by_token
     @user = User.find_by_token_for(:password_reset, params[:token])
-    redirect_to new_password_reset_path, alert: "Token Expired. Please try again.", unless @user.present?
+    redirect_to new_password_reset_path, alert: "Token Expired. Please try again." unless @user.present?
   end
 
   def password_params
-    params.require(:user).permit(:password)
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
